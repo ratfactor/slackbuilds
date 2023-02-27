cc323f74f5f0aac05ac89fb251fb667a3e6b9813
Dave Gauer <dave@ratfactor.com>
2023-02-27 08:31:09 -0500

    Initial commit with 'qbe'

---
+++ tar-it-up.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [[ -z $1 ]]
+then
+    echo "Usage: tar-it-up.sh <directory>"
+    exit
+fi
+
+out=$1.tar.gz
+echo "Making it (tar czf $out $1): '$1'..."
+tar czf $out $1
+echo "Testing (tar tf $out)..."
+echo "----------------------------------------------------"
+tar tf $out
+echo "----------------------------------------------------"
+echo "Done! (You can extract with 'tar xf $out'). Enjoy!."
